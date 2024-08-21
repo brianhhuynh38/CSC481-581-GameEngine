f124da5eed9f7074d84242846083da96eca5f850
brianhhuynh38 <bhhuynh@ncsu.edu>
2024-08-21 09:45:32 -0400

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+	// Temporary setup message, delete later
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Hello", "It works", NULL);
+	return 0;
+}
