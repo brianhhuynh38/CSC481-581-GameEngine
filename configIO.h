e86f2a802bf801b7816eb863a4df4489f54359c3
brianhhuynh38 <brianhhuynh38@gmail.com>
2024-10-02 14:07:13 -0400

    Updated Config to be a struct, all other changes errored too many things so this is it

--- configIO.h
+++ configIO.h
@@ -3,10 +3,18 @@
 #ifndef CONFIGIO_H
 #define CONFIGIO_H
 
+// Stores configuration settings
+typedef struct ConfigSettings {
+	/// The resolution width the window is set to
+	int resolutionWidth;
+	/// The resolution Height the window is set to
+	int resolutionHeight;
+};
+
 /**
 * Loads variables defined in the config.ini file and sets them to their proper places
 */
-void loadConfigFile(void);
+void loadConfigFile(ConfigSettings *settings);
 
 
 #endif
